94fccdc351ce9419d73e4f99ea79bfda0c462ce2
wen <439023672@qq.com>
2025-03-29 14:47:10 +0800

    初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="homeLayout">
+		<view class="banner">
+			<swiper>
+				<swiper-item v-for>
+					<image src="/commom/images/banner1.jpg" mode=""></image>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
