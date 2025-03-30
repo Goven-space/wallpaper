d6d40bb0b2399f5a61c73828e21fef4bff10bcf9
wen <439023672@qq.com>
2025-03-31 01:01:41 +0800

    轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,19 +1,50 @@
 <template>
 	<view class="homeLayout">
 		<view class="banner">
-			<swiper>
-				<swiper-item v-for>
-					<image src="/commom/images/banner1.jpg" mode=""></image>
+			<swiper circular indicator-dots indicator-color="rgba(255,255,255,0.5)" indicator-active-color="#fff" autoplay>
+				<swiper-item v-for="item in 3">
+					<image src="/commom/images/banner1.jpg" mode="aspectFill"></image>
 				</swiper-item>
 			</swiper>
+		</view>
+		
+		<view class="notice">
+			<view class="left">
+				<uni-icons type="sound-filled" size="20" color="#28b389"></uni-icons>
+				<text class="text">公告</text>
+			</view>
+			<view class="center"></view>
+			<view class="right">
+				<uni-icons type="right" size="16" color="#333"></uni-icons>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script setup>
-	
+
 </script>
 
 <style lang="scss" scoped>
+	.homeLayout {
+		.banner {
+			width: 750rpx;
+			padding: 30rpx 0;
+			swiper{
+				width: 750rpx;
+				height: 340rpx;
+				&-item{
+					width: 100%;
+					height: 100%;
+					padding: 0 30rpx;
+					image{
+						width: 100%;
+						height: 100%;
+						border-radius: 10rpx;
+					}
+				}
+			}
+		}
 	
+	}
 </style>
